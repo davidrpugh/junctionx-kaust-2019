9ae9e2754b5d9fefc78cd914ceb5bdee7b152de1
ilies <iliesaya@gmail.com>
2019-09-14 11:08:09 +0300

    updating server flask

--- notebooks/db/update_historian.py
+++ notebooks/db/update_historian.py
@@ -1,6 +1,4 @@
-
 #%%
-
 
 # from libs.Grafana.config import Config
 # from libs.Grafana.dbase import Database
@@ -9,8 +7,6 @@ import pandas as pd
 import numpy as np
 import datetime
 import time
-
-
 
 
 
@@ -145,100 +141,78 @@ class Database(object):
 
 #%%
 dbase = Database()  
-#%%
-
-data_dir = './data/raw/'
-fname = os.path.join(data_dir, 'neom-data.csv')
-
-df = pd.read_csv(fname, header = 0, error_bad_lines=False)
-#df = df.rename(columns={'Unnamed: 0': 'date'})
-df.columns = df.columns.str.replace('Unnamed: 0','date')
-df['date'] = pd.to_datetime(df['date'], format='%m/%d/%y %H:%M')
-df['Day'] = df["date"].dt.dayofyear
-df['Hour'] = df["date"].dt.hour
-columns = [
- 'mslp(hPa)',
- 't2(C)',
- 'td2(C)',
- 'wind_speed(m/s)',
- 'wind_dir(Deg)',
- 'rh(%)',
- 'GHI(W/m2)',
- 'SWDIR(W/m2)',
- 'SWDNI(W/m2)',
- 'SWDIF(W/m2)',
- 'rain(mm)',
- 'AOD']#'date',
-#%%
-for i in range(1,df.shape[0]):
-    print(f" date:{df.iloc[i]['date']} ")#, temp:{df[i]['t2(C)']}
-    date = df.iloc[i]['date']
-    for c in columns:
-        dbase.post(now=date, tag_dict={"ID":c}, seriesName="csv_import", value=df.iloc[i][c])
-    
-#%%
-
-values_mslp = []
-for i in range(1,df.shape[0]):
-    date = df.iloc[i]['date']
-    value = df.iloc[i]["mslp(hPa)"]
-    values_mslp.append({"date": date, "value":value})
-
-
-#%%
-dbase.postArray(tag_dict={"ID":"mslp(hPa)"}, seriesName="csv_import", values=values_mslp)
 
 
 #%%
 
-columns = [
- "mslp(hPa)",
- 't2(C)',
- 'td2(C)',
- 'wind_speed(m/s)',
- 'wind_dir(Deg)',
- 'rh(%)',
- 'GHI(W/m2)',
- 'SWDIR(W/m2)',
- 'SWDNI(W/m2)',
- 'SWDIF(W/m2)',
- 'rain(mm)',
- 'AOD']
-for c in columns:
-    values = []
-    for i in range(1,df.shape[0]):
-        date = df.iloc[i]['date']
-        value = df.iloc[i][c]
-        values.append({"date": date, "value":value})
-    print(f"inserting column {c}")
-    dbase.postArray(tag_dict={"ID":c}, seriesName="csv_import", values=values)
-    
+import pyowm
+#https://github.com/csparpa/pyowm
+#https://pyowm.readthedocs.io/en/latest/usage-examples-v2/weather-api-usage-examples.html#owm-weather-api-version-2-5-usage-examples
+owm = pyowm.OWM('68f8c0b152aa2c29c1f6123f3cdb4760')  # You MUST provide a valid API key
+
+# Have a pro subscription? Then use:
+# owm = pyowm.OWM(API_key='your-API-key', subscription_type='pro')
+
+
+# Search for current weather in London (Great Britain)
+observation = owm.weather_at_place('London,GB')
+w = observation.get_weather()
+print(w)
 
 #%%
-
-data_dir = './results/'
-fname = os.path.join(data_dir, 'actual-vs-predicted-values-for-2018.csv')
-
-df = pd.read_csv(fname, header = 0, error_bad_lines=False)
-#df = df.rename(columns={'Unnamed: 0': 'date'})
-#df.columns = df.columns.str.replace('Unnamed: 0','date')
-df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+# Weather details
+print(w.get_wind()     )             # {'speed': 4.6, 'deg': 330}
+print(w.get_humidity()  )            # 87
+print(w.get_temperature('celsius'))  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
 
 #%%
-df
-
+#neom gps 
+#observation = owm.weather_around_coords(28.239313, 34.736139)
 #%%
+
+#w = observation[0].get_weather()
+#%%
+
+
+print(w.get_reference_time(timeformat='date'))
+print(w.get_clouds())
+print(w.get_rain())
+print(w.get_wind())
+print(w.get_humidity())
+print(w.get_pressure())
+print(w.get_temperature(unit='celsius'))
+print(w.get_sunrise_time())
+print(w.get_sunset_time('iso'))
+
+# 2019-09-13 20:09:52+00:00
+# 0
+# {}
+# {'speed': 6.2, 'deg': 90}
+# 72
+# {'press': 1035, 'sea_level': None}
+# {'temp': 14.03, 'temp_max': 17.0, 'temp_min': 11.11, 'temp_kf': None}
+# 1568352700
+# 2019-09-13 18:21:49+00
+#%%
+#riyad
+from datetime import datetime
+
+now = datetime.now()
+print(now)
+#%%
+
+#updating humidity historian
 values = []
-for i in range(1,df.shape[0]):
-    date = df.iloc[i]['Timestamp']
-    value = df.iloc[i]["Predicted Solar Power (W/m2)"]
-    values.append({"date": date, "value":value})
+date = now
+value = w.get_humidity()
+values.append({"date": date, "value":value})
 
 #%%
 values
 
 #%%
-dbase.postArray(tag_dict={"ID":"Predicted Solar Power (W/m2)"}, seriesName="csv_import", values=values)
 
+#this will insert in influxDB the humidity 
+dbase.postArray(tag_dict={"ID":"rh(%)"}, seriesName="csv_import", values=values)
 
-#%%
+#do the same with all other mertrics ...
---
+++ src/go_flask.py
@@ -0,0 +1,119 @@
+#%%
+from flask import Flask
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn import ensemble, preprocessing
+
+
+MODEL = joblib.load("../models/tuned-random-forest-regression-model.pkl")
+
+
+def preprocess_features(df):
+    _numeric_features = ["GHI(W/m2)",
+                         "mslp(hPa)",
+                         "rain(mm)",
+                         "rh(%)",
+                         "t2(C)",
+                         "td2(C)",
+                         "wind_dir(Deg)",
+                         "wind_speed(m/s)"]
+
+    _ordinal_features = ["AOD",
+                         "day",
+                         "month",
+                         "year"]
+
+    standard_scalar = preprocessing.StandardScaler()
+    Z0 = standard_scalar.fit_transform(df.loc[:, _numeric_features])
+    
+    ordinal_encoder = preprocessing.OrdinalEncoder()
+    Z1 = ordinal_encoder.fit_transform(df.loc[:, _ordinal_features])
+    transformed_features = np.hstack((Z0, Z1))
+    
+    return transformed_features
+
+
+def feature_engineering(df):
+    _dropped_cols = ["SWDIR(W/m2)", "SWDNI(W/m2)", "SWDIF(W/m2)"]
+
+    _year = (df.index
+               .year)
+    _month = (df.index
+                .month)
+    _day = (df.index
+              .dayofyear)
+    _hour = (df.index
+               .hour)
+
+    features = (df.drop(_dropped_cols, axis=1, inplace=False)
+                  .assign(year=_year, month=_month, day=_day, hour=_hour)
+                  .groupby(["year", "month", "day", "hour"])
+                  .mean()
+                  .unstack(level=["hour"])
+                  .reset_index(inplace=False)
+                  .sort_index(axis=1)
+                  .drop("year", axis=1, inplace=False))
+    
+    # create the proxy for our solar power target
+    efficiency_factor = 0.5
+    target = (features.loc[:, ["GHI(W/m2)"]]
+                      .mul(efficiency_factor)
+                      .shift(-1)
+                      .rename(columns={"GHI(W/m2)": "target(W/m2)"}))
+
+    # combine to create the input data
+    input_data = (features.join(target)
+                      .dropna(how="any", inplace=False)
+                      .sort_index(axis=1))
+    return input_data
+
+
+# load data
+neom_data = (pd.read_csv("../data/raw/neom-data.csv", parse_dates=[0], nrows=100)
+                .rename(columns={"Unnamed: 0": "Timestamp"})
+                .set_index("Timestamp", drop=True, inplace=False))
+
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET',])
+def home():
+    #return '<pre>'+get_shell_script_output_using_check_output()+'</pre>'
+    
+     # perform feature engineering
+    input_data = feature_engineering(neom_data)
+    
+    # simulate online learning by sampling features from the input data
+    _prng = np.random.RandomState(42)
+    new_features = input_data.sample(n=1, random_state=_prng)
+    
+    # perform inference
+    processed_features = preprocess_features(new_features)
+    predictions = MODEL.predict(processed_features)
+    
+    # print the total solar power produced
+    print(predictions.sum())
+    
+    res = """<html>
+            <head>
+                <title>Neom Energy cost</title>
+            </head>
+            <body bgcolor="#ff0000">
+                <h1>Tomorrow energy flag is RED : {}</h1>
+            </body>
+            </html>""".format(predictions.sum())
+    
+    
+    
+    
+    return res
+
+
+
+app.run(host='0.0.0.0',port=5000, debug=True)
+
+
+#%%
+if __name__=="__main__":
+     app.run(host='0.0.0.0', port=5000, debug=True)
